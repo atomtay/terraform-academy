7d1da2cad753b67b3438f555f35cf21363ba8b11
Annabelle Thomas Taylor <annabelle-taylor@pluralsight.com>
2022-07-05 09:23:01 -0400

    set workflows and rds with public subnets

--- infra/rds.tf
+++ infra/rds.tf
@@ -38,20 +38,26 @@ resource "aws_db_subnet_group" "default" {
   subnet_ids = [for subnet in aws_subnet.main : subnet.id]
 }
 
+resource "aws_db_subnet_group" "public" {
+  subnet_ids = [for subnet in aws_subnet.public : subnet.id]
+}
+
 resource "aws_db_instance" "postgres" {
   #checkov:skip=CKV_AWS_17:Create public IP because we don't have access to private GH Actions runners
-  apply_immediately      = true
-  engine                 = "postgres"
-  engine_version         = "13.4"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  password               = random_password.postgres_admin_password.result
-  username               = "myadmin"
-  port                   = 5432
-  publicly_accessible    = true
-  multi_az               = true
-  db_subnet_group_name   = aws_db_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.postgres.id]
+  apply_immediately         = true
+  db_name                   = "todo"
+  engine                    = "postgres"
+  engine_version            = "13.4"
+  instance_class            = "db.t3.micro"
+  allocated_storage         = 5
+  password                  = random_password.postgres_admin_password.result
+  username                  = "myadmin"
+  port                      = 5432
+  publicly_accessible       = true
+  multi_az                  = true
+  db_subnet_group_name      = aws_db_subnet_group.public.name
+  vpc_security_group_ids    = [aws_security_group.postgres.id]
+  final_snapshot_identifier = "last-snapshot"
 
   lifecycle {
     ignore_changes = [
